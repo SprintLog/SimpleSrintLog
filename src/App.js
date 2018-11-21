24e1a795cf1f6af84d7c0d7bc96770e1ddddc009
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-11-21 15:41:47 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./containners/Navigation";
 class App extends Component {
   render() {
     return (
-   
+      <React.Fragment>
+        <Navigation />
+        <div className="container content-container">
+          asdasda
+        </div>
+      </React.Fragment>
     );
   }
 }
